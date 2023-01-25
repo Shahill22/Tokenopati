800c18f964d212377652d5badb38535b90d0534d
Acsah Alex <acsah.alex@accubits.com>
2023-01-25 20:01:30 +0530

    updated deploy scripts

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,4 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 require("dotenv").config();
 
 const mnemonic = process.env.MNEMONIC;
@@ -14,6 +14,18 @@ module.exports = {
       port: 8545,
       network_id: "*",
       gas: 6721975,
+    },
+    goerli: {
+      networkCheckTimeout: 60000,
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://goerli.infura.io/ws/v3/${infuraProjectId}`
+        ),
+      network_id: 5,
+      confirmations: 2,
+      timeoutBlocks: 1000,
+      skipDryRun: true,
     },
     rinkeby: {
       networkCheckTimeout: 60000,
@@ -40,22 +52,26 @@ module.exports = {
       skipDryRun: true,
     },
     avalanche_testnet: {
-      provider: () => new HDWalletProvider(mnemonic, `https://api.avax-test.network/ext/bc/C/rpc`),
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://api.avax-test.network/ext/bc/C/rpc`
+        ),
       network_id: 43113,
       gas: 5500000,
       confirmations: 2,
       timeoutBlocks: 200,
-      skipDryRun: true
+      skipDryRun: true,
     },
     bsc_testnet: {
       provider: function () {
         return new HDWalletProvider({
           mnemonic: {
-            phrase: mnemonic
+            phrase: mnemonic,
           },
           providerOrUrl: `https://data-seed-prebsc-1-s1.binance.org:8545`,
           chainId: 97,
-        })
+        });
       },
       network_id: 97,
       // confirmations: 10,
@@ -67,11 +83,11 @@ module.exports = {
       provider: function () {
         return new HDWalletProvider({
           mnemonic: {
-            phrase: mnemonic
+            phrase: mnemonic,
           },
           providerOrUrl: `https://bsc-dataseed1.binance.org`,
           chainId: 56,
-        })
+        });
       },
       network_id: 56,
       confirmations: 10,
@@ -84,23 +100,22 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.17",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.17", // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      settings: {          // See the solidity docs for advice about optimization and evmVersion
+      settings: {
+        // See the solidity docs for advice about optimization and evmVersion
         optimizer: {
           enabled: true,
-          runs: 200
+          runs: 200,
         },
-        evmVersion: "byzantium"
-      }
-    }
+        evmVersion: "byzantium",
+      },
+    },
   },
-  plugins: [
-    'truffle-plugin-verify'
-  ],
+  plugins: ["truffle-plugin-verify"],
   api_keys: {
     etherscan: etherscanApiKey,
     snowtrace: snowtraceApiKey,
-    bscscan: bscscanApiKey
-  }
+    bscscan: bscscanApiKey,
+  },
 };
