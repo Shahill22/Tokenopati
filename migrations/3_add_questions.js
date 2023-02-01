d84ecbe5cd405d4c9e25d47c71c6c6e773edd06c
Beno Sam Binu <benosam.binu@accubits.com>
2023-02-01 17:13:25 +0530

    updated add questions script

--- migrations/3_add_questions.js
+++ migrations/3_add_questions.js
@@ -3,14 +3,14 @@ const Cryptopati = artifacts.require("Cryptopati");
 
 module.exports = async function (deployer, network, accounts) {
     const cryptopati = await Cryptopati.deployed();
-    const questionTypes = 3, questionCount = 12
-    const multiplier = [2, 4, 6]
+    const questionTypes = 3, questionCount = 21
+    const multiplier = [2, 5, 10]
     let questionCounter = 1;
 
     for (let i = 0; i < questionTypes; i++) {
         for (let j = 0; j < questionCount; j++) {
             let questionId = questionCounter.toString()
-            cryptopati.addQuestion(questionId, multiplier[i]);
+            await cryptopati.addQuestion(questionId, multiplier[i]);
             questionCounter++;
         }
     }
